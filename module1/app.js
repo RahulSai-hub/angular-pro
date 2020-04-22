8b8d134aad587707701a6ef88ef90605913168de
Rahul <rahulsai@basina.com>
2020-04-22 17:46:31 +0530

    module1

---
+++ module1/app.js
@@ -0,0 +1,34 @@
+(
+    function () {
+        angular.module('LunchCheck',[])
+        .controller('LunchCheckController', function1 );
+        function1.$inject = ['$scope'];
+        function function1($scope) {
+            $scope.lunch = '';
+            $scope.message = '';
+            $scope.function2 = function (){
+                let arrayOfItems = $scope.lunch.split(',');
+                let counter = 0;
+
+                arrayOfItems.forEach((item) => {
+                    if (item != false) {
+                        counter++;
+                    }
+                });
+
+                if (counter > 0 && counter < 4){
+                    $scope.message = 'Enjoy!';
+
+                }else if(counter >= 4){
+                    $scope.message = 'Too much!';
+
+
+                }else{
+                    $scope.message = 'Please enter data first';
+
+                }
+            }
+        }
+
+    }
+)()
